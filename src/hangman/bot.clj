26e0fe0feccc4af673445bac3f63df27764145a5
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2017-03-05 13:20:08 +0100

    Print state

--- src/hangman/bot.clj
+++ src/hangman/bot.clj
@@ -93,7 +93,9 @@
     (cond
       (= (count message-text) 1)
       (let [updated-guesses (conj (get state :guesses) (first (seq message-text)))]
-        (do (fb/send-message sender-id (fb/text-message (str "OK, carry on: " (mask (get state :word) updated-guesses))))
+        (do
+            (println (str "State is " state))
+            (fb/send-message sender-id (fb/text-message (str "OK, carry on: " (mask (get state :word) updated-guesses))))
             (update (assoc state :guesses updated-guesses))))
 
       ; If no rules apply echo the user's message-text input
