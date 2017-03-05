58b95701f91aded1f3fb6aabd3527b6f1b918da9
Lorenz Leutgeb <lorenz.leutgeb@gmail.com>
2017-03-05 13:15:45 +0100

    Use do

--- src/hangman/bot.clj
+++ src/hangman/bot.clj
@@ -93,8 +93,8 @@
     (cond
       (= (count message-text) 1)
       (let [updated-guesses (conj (get state :guesses) (first (seq message-text)))]
-        (doall (fb/send-message sender-id (fb/text-message (str "OK, carry on: " (mask (get state :word) updated-guesses))))
-               (update (assoc state :guesses updated-guesses))))
+        (do (fb/send-message sender-id (fb/text-message (str "OK, carry on: " (mask (get state :word) updated-guesses))))
+            (update (assoc state :guesses updated-guesses))))
 
       ; If no rules apply echo the user's message-text input
       :else
